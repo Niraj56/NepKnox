7eb3f0da007d69b4ade21fd9fd861aeb374f2397
Niraj Tiwari <43477992+Niraj56@users.noreply.github.com>
2020-02-17 21:02:07 -0500

    Add files via upload

---
+++ admob.js
@@ -0,0 +1,308 @@
+cordova.define("cordova-plugin-ad-admob.admob", function(require, exports, module) {
+
+module.exports = {
+	_loadedBannerAd: false,
+	_loadedInterstitialAd: false,
+	_loadedRewardedVideoAd: false,	
+	_isShowingBannerAd: false,
+	_isShowingInterstitialAd: false,
+	_isShowingRewardedVideoAd: false,	
+	_fixCocoonIOCordovaAndroidAdMobIssue: false,
+	//
+	setLicenseKey: function(email, licenseKey) {
+		var self = this;	
+        cordova.exec(
+            null,
+            null,
+            'AdMobPlugin',
+            'setLicenseKey',			
+            [email, licenseKey]
+        ); 
+    },
+	setUp: function(bannerAdUnit, interstitialAdUnit, rewardedVideoAdUnit, isOverlap, isTest) {
+        if (typeof isTest == 'undefined') {
+            //isOverlap=rewardedVideoAdUnit;
+            //isTest=isOverlap;
+			rewardedVideoAdUnit_=rewardedVideoAdUnit;
+			isOverlap_=isOverlap;
+			isTest_=isTest;
+			//
+			isOverlap=rewardedVideoAdUnit_;
+            isTest=isOverlap_;			
+        }
+        
+		var self = this;
+        cordova.exec(
+            function (result) {
+				if (typeof result == "string") {
+					if (result == "onBannerAdPreloaded") {					
+						if (self.onBannerAdPreloaded)
+							self.onBannerAdPreloaded();
+					}
+					else if (result == "onBannerAdLoaded") {
+						self._loadedBannerAd = true;
+						
+						if (self.onBannerAdLoaded)
+							self.onBannerAdLoaded();
+														
+//fixCocoonIOCordovaAndroidAdMobIssue
+if (typeof Cocoon != 'undefined' && navigator.userAgent.match(/Android/i) && !self._fixCocoonIOCordovaAndroidAdMobIssue) {
+	self.reloadBannerAd();
+	self._fixCocoonIOCordovaAndroidAdMobIssue=true;
+}							
+					}
+					else if (result == "onBannerAdShown") {
+						self._loadedBannerAd = false;
+						self._isShowingBannerAd = true;
+					
+						if (self.onBannerAdShown)
+							self.onBannerAdShown();
+					}
+					else if (result == "onBannerAdHidden") {
+						self._isShowingBannerAd = false;
+					
+						 if (self.onBannerAdHidden)
+							self.onBannerAdHidden();
+					}
+					//
+					else if (result == "onInterstitialAdPreloaded") {
+//cranberrygame start; deprecated					
+						if (self.onFullScreenAdPreloaded)
+							self.onFullScreenAdPreloaded();
+//cranberrygame end							
+						if (self.onInterstitialAdPreloaded)
+							self.onInterstitialAdPreloaded();							
+					}
+					else if (result == "onInterstitialAdLoaded") {
+						self._loadedInterstitialAd = true;
+
+//cranberrygame start; deprecated						
+						if (self.onFullScreenAdLoaded)
+							self.onFullScreenAdLoaded();
+//cranberrygame end							
+						if (self.onInterstitialAdLoaded)
+							self.onInterstitialAdLoaded();							
+					}
+					else if (result == "onInterstitialAdShown") {
+						self._loadedInterstitialAd = false;					
+						self._isShowingInterstitialAd = true;
+
+//cranberrygame start; deprecated					
+						if (self.onFullScreenAdShown)
+							self.onFullScreenAdShown();
+//cranberrygame end						
+						if (self.onInterstitialAdShown)
+							self.onInterstitialAdShown();							
+					}
+					else if (result == "onInterstitialAdHidden") {
+						self._isShowingInterstitialAd = false;
+
+//cranberrygame start; deprecated					
+						if (self.onFullScreenAdHidden)
+							self.onFullScreenAdHidden();
+						if (self.onFullScreenAdClosed)
+							self.onFullScreenAdClosed(); //deprecated	
+//cranberrygame end							
+						 if (self.onInterstitialAdHidden)
+							self.onInterstitialAdHidden();
+					}
+					//
+					else if (event == "onRewardedVideoAdPreloaded") {
+						if (self.onRewardedVideoAdPreloaded)
+							self.onRewardedVideoAdPreloaded();
+					}
+					else if (event == "onRewardedVideoAdLoaded") {
+						self._loadedRewardedVideoAd = true;
+
+						if (self.onRewardedVideoAdLoaded)
+							self.onRewardedVideoAdLoaded();
+					}
+					else if (event == "onRewardedVideoAdShown") {
+						self._loadedRewardedVideoAd = false;
+						self._isShowingRewardedVideoAd = true;
+					
+						if (self.onRewardedVideoAdShown)
+							self.onRewardedVideoAdShown();
+					}
+					else if (event == "onRewardedVideoAdHidden") {
+						self._isShowingRewardedVideoAd = false;
+					
+						 if (self.onRewardedVideoAdHidden)
+							self.onRewardedVideoAdHidden();
+					}
+					else if (event == "onRewardedVideoAdCompleted") {
+						if (self.onRewardedVideoAdCompleted)
+							self.onRewardedVideoAdCompleted();
+					}					
+				}
+				else {
+					//var event = result["event"];
+					//var location = result["message"];				
+					//if (event == "onXXX") {
+					//	if (self.onXXX)
+					//		self.onXXX(location);
+					//}
+				}			
+			}, 
+			function (error) {
+			},
+            'AdMobPlugin',
+            'setUp',			
+            [bannerAdUnit, interstitialAdUnit, rewardedVideoAdUnit, isOverlap, isTest]
+        ); 
+    },
+	preloadBannerAd: function() {
+		var self = this;	
+        cordova.exec(
+            null,
+            null,
+            'AdMobPlugin',
+            'preloadBannerAd',
+            []
+        ); 
+    },
+    showBannerAd: function(position, size) {
+		var self = this;	
+        cordova.exec(
+            null,
+            null,
+            'AdMobPlugin',
+            'showBannerAd',
+            [position, size]
+        ); 
+    },
+	reloadBannerAd: function() {
+		var self = this;	
+        cordova.exec(
+            null,
+            null,
+            'AdMobPlugin',
+            'reloadBannerAd',
+            []
+        ); 
+    },
+    hideBannerAd: function() {
+		var self = this;
+        cordova.exec(
+            null,
+            null,
+            'AdMobPlugin',
+            'hideBannerAd',
+            []
+        ); 
+    },
+	//
+//cranberrygame start; deprecated	
+	preloadFullScreenAd: function() {
+		var self = this;	
+        cordova.exec(
+            null,
+            null,
+            'AdMobPlugin',
+            'preloadInterstitialAd',
+            []
+        ); 
+    },
+    showFullScreenAd: function() {
+		var self = this;
+		cordova.exec(
+            null,
+            null,
+            'AdMobPlugin',
+            'showInterstitialAd',
+            []
+        ); 
+    },
+	reloadFullScreenAd: function() { //deprecated
+    },
+//cranberrygame end	
+	preloadInterstitialAd: function() {
+        cordova.exec(
+			null,
+            null,
+            'AdMobPlugin',
+            'preloadInterstitialAd',
+            []
+        ); 
+    },
+    showInterstitialAd: function() {
+		cordova.exec(
+ 			null,
+            null,
+            'AdMobPlugin',
+            'showInterstitialAd',
+            []
+        ); 
+    },
+	//
+	preloadRewardedVideoAd: function() {
+        cordova.exec(
+			null,
+            null,
+            'AdMobPlugin',
+            'preloadRewardedVideoAd',
+            []
+        ); 
+    },
+    showRewardedVideoAd: function() {
+		cordova.exec(
+			null,
+            null,
+            'AdMobPlugin',
+            'showRewardedVideoAd',
+            []
+        ); 
+    },	
+	loadedBannerAd: function() {
+		return this._loadedBannerAd;
+	},
+//cranberrygame start; deprecated	
+	loadedFullScreenAd: function() {
+		return this._loadedInterstitialAd;
+	},
+//cranberrygame end	
+	loadedInterstitialAd: function() {
+		return this._loadedInterstitialAd;
+	},
+	loadedRewardedVideoAd: function() {
+		return this._loadedRewardedVideoAd;
+	},	
+	isShowingBannerAd: function() {
+		return this._isShowingBannerAd;
+	},
+//cranberrygame start; deprecated	
+	isShowingFullScreenAd: function() {
+		return this._isShowingInterstitialAd;
+	},
+//cranberrygame end	
+	isShowingInterstitialAd: function() {
+		return this._isShowingInterstitialAd;
+	},
+	isShowingRewardedVideoAd: function() {
+		return this._isShowingRewardedVideoAd;
+	},	
+	onBannerAdPreloaded: null,
+	onBannerAdLoaded: null,
+	onBannerAdShown: null,
+	onBannerAdHidden: null,	
+	//
+//cranberrygame start; deprecated	
+	onFullScreenAdPreloaded: null,
+	onFullScreenAdLoaded: null,
+	onFullScreenAdShown: null,
+	onFullScreenAdHidden: null,
+	onFullScreenAdClosed: null, //deprecated		
+//cranberrygame end
+	onInterstitialAdPreloaded: null,
+	onInterstitialAdLoaded: null,
+	onInterstitialAdShown: null,
+	onInterstitialAdHidden: null,
+	//
+	onRewardedVideoAdPreloaded: null,
+	onRewardedVideoAdLoaded: null,
+	onRewardedVideoAdShown: null,
+	onRewardedVideoAdHidden: null,
+	onRewardedVideoAdCompleted: null
+};
+
+});
